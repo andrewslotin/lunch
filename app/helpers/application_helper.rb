f501cdd1a936f050c551cc79292781c476ed254f
Andrew Slotin <andrew.slotin@gmail.com>
2013-01-29 17:34:17 +0100

    Refactored places

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  WALKING_SPEED = 5.5
+  RUNNING_SPEED = 12.0
+
+  def link_to_venue(venue)
+    link_to venue.name, venue.url
+  end
+
+  def walking_time(distance)
+    (distance / WALKING_SPEED).ceil
+  end
+
+  def running_time(distance)
+    (distance / RUNNING_SPEED).ceil
+  end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,40 @@
 # -*- encoding : utf-8 -*-
 
 class Place
+  include Gmaps4rails::ActsAsGmappable
+
+  attr_reader :name, :distance, :url, :category, :lat, :lng
+
   def self.around(lat, lng, radius = 500)
-    foursquare.explore_venues(ll: [lat, lng].join(","), section: :food, radius: radius)
+    foursquare_results = if Rails.env.development?
+                           JSON.load(File.read("data.json"))
+                         else
+                           foursquare.explore_venues(ll: [lat, lng].join(","), section: :food, radius: radius)
+                         end
+    foursquare_results["groups"].map do |group|
+      group["items"].map do |venue|
+        self.new(venue["venue"])
+      end
+    end.flatten
+  end
+
+  def initialize(foursquare_hash)
+    @name = foursquare_hash["name"]
+    @url = foursquare_hash["canonicalUrl"]
+    @lat = foursquare_hash["location"]["lat"]
+    @lng = foursquare_hash["location"]["lng"]
+    @distance = foursquare_hash["location"]["distance"]
+
+    address_fields = %w(address city state country postalCode)
+    @address = Hash[address_fields.map(&:to_sym).zip(foursquare_hash["location"].values_at(*address_fields))]
+  end
+
+  def address
+    [@address[:address], city_with_zip].select(&:presence).join(", ")
+  end
+
+  def city_with_zip
+    [@address[:postalCode], @address[:city]].select(&:presence).join(" ") if @address[:city].present?
   end
 
   private

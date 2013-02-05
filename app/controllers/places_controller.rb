5635d6cbba6c8810aa73781bd6cbec94cd95e666
Andrew Slotin <andrew.slotin@gmail.com>
2013-02-05 23:48:13 +0100

    Ordering places by rating

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < InheritedResources::Base
   def index
     lat = "52.49402952426317"
     lng = "13.429402830064818"
-    @venues = Place.around(lat, lng)
+    @venues = Place.around(lat, lng).desc(:rating)
   end
 
   def vote
